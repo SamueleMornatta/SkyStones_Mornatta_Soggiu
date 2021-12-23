31b30948f0d686418c2355a594e00504d6379a60
MAALFING <marco.soggiu@live.it>
2021-12-23 17:33:11 +0100

    Creazione classi Upgrade e Card

---
+++ SkyStones/SkyStones/Card.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace SkyStones
+{
+    class Card
+    {
+        private int ID;
+        private int[] att;
+        private BitmapImage I;
+        private String tipo;
+
+
+        public Card()
+        {
+            ID = 0;
+            att = new int[4];
+            for (int i = 0; i < att.Length; i++)
+            {
+                att[i] = 0;
+            }
+            I = new BitmapImage();
+            tipo = "";
+        }
+
+        public Card(int iD, int[] att, BitmapImage i, string tipo)
+        {
+            ID = iD;
+            this.att = att;
+            I = i;
+            this.tipo = tipo;
+        }
+
+        int getID()
+        {
+            return ID;
+        }
+
+        BitmapImage getI()
+        {
+            return I;
+        }
+        int[] getAtt()
+        {
+            return att;
+        }
+        string getTipo()
+        {
+            return tipo;
+        }
+        
+        void setID(int ID)
+        {
+            this.ID = ID;
+        }
+
+        void setI(BitmapImage I)
+        {
+            this.I = I;
+        }
+
+        void setAtt(int[] att)
+        {
+            this.att = att;
+        }
+
+        void setTipo(string tipo)
+        {
+            this.tipo = tipo;
+        }
+
+
+    }
+}
---
+++ SkyStones/SkyStones/Upgrade.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkyStones
+{
+    class Upgrade
+    {
+        private int ID;
+        private string desc;
+
+        public Upgrade()
+        {
+            ID = 0;
+            desc = "";
+        }
+
+        public Upgrade(int iD, string desc)
+        {
+            ID = iD;
+            this.desc = desc;
+        }
+    }
+}
