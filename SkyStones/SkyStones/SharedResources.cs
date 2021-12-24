29ae98d6503bb3b8c593cb56b4df31bf3efecbbb
Samuele Mornatta <samuelemornatta@gmail.com>
2021-12-24 14:22:37 +0100

    Aggiunti metodi per caricare tutte le carte e potenziamenti con JSON

--- SkyStones/SkyStones/SharedResources.cs
+++ SkyStones/SkyStones/SharedResources.cs
@@ -1,16 +1,24 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
 
 namespace SkyStones
 {
     class SharedResources
     {
         private static SharedResources _instance;
+        private List<Card> collection;
+        private List<Upgrade> upgrades;
         private static readonly Object _sync = new Object();
-        private SharedResources() { }
+        private SharedResources() {
+            collection = ReadAllCards();
+            upgrades = ReadAllUpgrades();
+        }
         public static SharedResources Instance
         {
             get
@@ -26,6 +34,42 @@ namespace SkyStones
                     }
                 }
                 return _instance;
+            }
+        }
+        public List<Card> ReadAllCards()
+        {
+            using (StreamReader r = new StreamReader("Cards.json"))
+            {
+                string json = r.ReadToEnd();
+                List<tempCard> items = JsonConvert.DeserializeObject<List<tempCard>>(json);
+                List<Card> cards = new List<Card>();
+                for (int i = 0; i < items.Count; i++)
+                {
+                    Card tmp = new Card();
+                    tmp.setID(items.ElementAt(i).ID);
+                    tmp.setAtt(items.ElementAt(i).att);
+                    tmp.setTipo(items.ElementAt(i).tipo);
+                    BitmapImage img = new BitmapImage(new Uri(items.ElementAt(i).Ipath));
+                    tmp.setI(img);
+                    cards.Add(tmp);
+                }
+                return cards;
+            }
+        }
+        public class tempCard
+        {
+            public int ID;
+            public int[] att;
+            public String Ipath;
+            public String tipo;
+        }
+        public List<Upgrade> ReadAllUpgrades()
+        {
+            using (StreamReader r = new StreamReader("Upgrades.json"))
+            {
+                string json = r.ReadToEnd();
+                List<Upgrade> items = JsonConvert.DeserializeObject<List<Upgrade>>(json);
+                return items;
             }
         }
     }
