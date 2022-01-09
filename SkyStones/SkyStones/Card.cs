5eff80de64d47d27e1533dcbbf3f162eb63452b9
MAALFING <marco.soggiu@live.it>
2022-01-09 01:03:56 +0100

    Spostamento della creazione dei canvas nella classe card

--- SkyStones/SkyStones/Card.cs
+++ SkyStones/SkyStones/Card.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
 using System.Windows.Media.Imaging;
 
 namespace SkyStones
@@ -15,6 +18,11 @@ namespace SkyStones
         public BitmapImage I { get; set; }
         public String tipo { get; set; }
         public String nome { get; set; }
+        public Canvas Can { get; set; }
+        private bool _isRectDragInProg;
+        private gameplay G;
+        private Label l { get; set; }
+        private Image Imm;
 
 
         public Card()
@@ -27,6 +35,8 @@ namespace SkyStones
             }
             I = new BitmapImage();
             tipo = "";
+            Can = new Canvas();
+            createCanvas();
         }
 
         public Card(int iD, int[] att, BitmapImage i, string tipo)
@@ -35,7 +45,132 @@ namespace SkyStones
             this.att = att;
             I = i;
             this.tipo = tipo;
+            Can = new Canvas();
+            createCanvas();
         }
+        public void setgameplay(gameplay G)
+        {
+            this.G = G;
+        }
+
+        public void createCanvas()
+        {
+            l = new Label();
+            Imm = new Image();
+            SolidColorBrush br = new SolidColorBrush();
+            br.Color = Colors.Gray;
+            Can = new Canvas();
+            Can.Height = 90;
+            Can.Width = 70;
+            Can.Background = br;
+
+
+            Can.MouseLeftButtonDown += (sender, e) => rect_MouseLeftButtonDown(sender, e);
+            Can.MouseLeftButtonUp += (sender, e) => rect_MouseLeftButtonUp(sender, e);
+            Can.MouseMove += (sender, e) => rect_MouseMove(sender, e);
+
+            Can.Children.Add(l);
+
+            Imm.Height = 20;
+            Imm.Width = 50;
+
+
+            if (att[0] != 0)
+            {
+                Imm.Source = new BitmapImage(new Uri(Directory.GetCurrentDirectory() + @"..\..\..\..\..\images\attN" + att[0] + ".png"));
+                var immagineN = Imm;
+                Canvas.SetTop(immagineN, -5);
+                Canvas.SetLeft(immagineN, 10);
+                Can.Children.Add(immagineN);
+            }
+            Imm = new Image();
+
+            Imm.Height = 20;
+            Imm.Width = 50;
+            if (att[1] != 0)
+            {
+                Imm.Source = new BitmapImage(new Uri(Directory.GetCurrentDirectory() + @"..\..\..\..\..\images\attS" + att[1] + ".png"));
+                var immagineS = Imm;
+                Canvas.SetBottom(immagineS, -5);
+                Canvas.SetLeft(immagineS, 10);
+                Can.Children.Add(immagineS);
+            }
+            Imm = new Image();
+
+
+            Imm.Height = 50;
+            Imm.Width = 20;
+
+            if (att[2] != 0)
+            {
+                Imm.Source = new BitmapImage(new Uri(Directory.GetCurrentDirectory() + @"..\..\..\..\..\images\attO" + att[2] + ".png"));
+                var immagineO = Imm;
+                Canvas.SetTop(immagineO, 20);
+                Canvas.SetLeft(immagineO, -5);
+                Can.Children.Add(immagineO);
+            }
+            Imm = new Image();
+
+            Imm.Height = 50;
+            Imm.Width = 20;
+            if (att[3] != 0)
+            {
+                Imm.Source = new BitmapImage(new Uri(Directory.GetCurrentDirectory() + @"..\..\..\..\..\images\attE" + att[3] + ".png"));
+                var immagineE = Imm;
+                Canvas.SetTop(immagineE, 20);
+                Canvas.SetRight(immagineE, -5);
+                Can.Children.Add(immagineE);
+            }
+            Imm = new Image();
+        }
+
+
+
+        private void rect_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            _isRectDragInProg = true;
+            Can.CaptureMouse();
+        }
+
+        private void rect_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            _isRectDragInProg = false;
+            Can.ReleaseMouseCapture();
+        }
+
+        private void rect_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!_isRectDragInProg) return;
+
+            // get the position of the mouse relative to the Canvas
+            var mousePos = e.GetPosition(G.tavola);
+
+            // center the rect on the mouse
+            double left = mousePos.X - (Can.ActualWidth / 2);
+            double top = mousePos.Y - (Can.ActualHeight / 2);
+            double bottom = mousePos.Y - (Can.ActualHeight / 2);
+            double right = mousePos.Y - (Can.ActualHeight / 2);
+            if (left >= 502)
+            {
+                left = 502;
+            }
+            else if (top >= 408)
+            {
+                top = 408;
+            }
+            else if (top <= 0)
+            {
+                top = 0;
+            }
+            else if (left <= 0)
+            {
+                left = 0;
+            }
+            G.coordinate.Content = "x: " + top + " y: " + left;
+            Canvas.SetLeft(Can, left);
+            Canvas.SetTop(Can, top);
+        }
+
 
         public int getID()
         {
--- SkyStones/SkyStones/gameplay.xaml.cs
+++ SkyStones/SkyStones/gameplay.xaml.cs
@@ -28,7 +28,6 @@ namespace SkyStones
         private StreamWriter writer;
         private StreamReader reader;
         private Thread t; //L'ho fatto io non ti preoccupare Pozzi
-        private bool _isRectDragInProg;
         private Canvas[,] matr = new Canvas[5, 5];
         private Label l = new Label();
         private Image Imm= new Image();
@@ -44,72 +43,19 @@ namespace SkyStones
             //reader = new StreamReader(stream);
             t = new Thread(new ThreadStart(listener));
             //t.Start();
-            int c = 0;
-            
-            
-            SolidColorBrush br = new SolidColorBrush();
-            br.Color = Colors.Gray;
-            
-            
+
+
+            //Application.Current.Dispatcher.Invoke(new Action(() => { MessageBox.Show(Directory.GetCurrentDirectory()+@"\..\.."); }));
+
+
             for (int i = 0; i < 5; i++)
             {
                 for (int j = 0; j < 5; j++)
                 {
-                    matr[i, j] = new Canvas();
-                    matr[i, j].Height = 90;
-                    matr[i, j].Width = 70;
-                    matr[i, j].Background = br;
-
-                    matr[i, j].MouseLeftButtonDown += rect_MouseLeftButtonDown;
-                    matr[i, j].MouseLeftButtonUp += rect_MouseLeftButtonUp;
-                    matr[i,j].MouseMove += rect_MouseMove;
-
-                    //matr[i, j].Children.Add(l);
-
-                    Imm.Height = 20;
-                    Imm.Width = 50;
-
-                    Imm.Source = new BitmapImage(new Uri(@"D:\Utenti\MAALFING\Documents\GitHub\SkyStones_Mornatta_Soggiu\images\attN4.png"));
-                    var immagineN = Imm;
-                    Canvas.SetTop(immagineN, 0);
-                    Canvas.SetLeft(immagineN, 10);
-                    matr[i, j].Children.Add(immagineN);
-                    Imm = new Image();
-
-                    Imm.Height = 20;
-                    Imm.Width = 50;
-
-                    Imm.Source = new BitmapImage(new Uri(@"D:\Utenti\MAALFING\Documents\GitHub\SkyStones_Mornatta_Soggiu\images\attS4.png"));
-                    var immagineS = Imm;
-                    Canvas.SetBottom(immagineS, 0);
-                    Canvas.SetLeft(immagineS, 10);
-                    matr[i, j].Children.Add(immagineS);
-                    Imm = new Image();
-
-
-                    Imm.Height = 50;
-                    Imm.Width = 20;
-
-                    Imm.Source = new BitmapImage(new Uri(@"D:\Utenti\MAALFING\Documents\GitHub\SkyStones_Mornatta_Soggiu\images\attO4.png"));
-                    var immagineO = Imm;
-                    Canvas.SetTop(immagineO, 20);
-                    Canvas.SetLeft(immagineO, 0);
-                    matr[i, j].Children.Add(immagineO);
-                    Imm = new Image();
-
-                    Imm.Height = 50;
-                    Imm.Width = 20;
-
-                    Imm.Source = new BitmapImage(new Uri(@"D:\Utenti\MAALFING\Documents\GitHub\SkyStones_Mornatta_Soggiu\images\attE4.png"));
-                    var immagineE = Imm;
-                    Canvas.SetTop(immagineE, 20);
-                    Canvas.SetRight(immagineE, 0);
-                    matr[i, j].Children.Add(immagineE);
-                    Imm = new Image();
-
-                    l.Content = c;
-                    l = new Label();
-                    c++;
+                    Card C = new Card();
+                    C.setgameplay(this);
+                    matr[i, j] = C.Can;                    
+                    
                 }
                 
             }
@@ -172,50 +118,7 @@ namespace SkyStones
         }
 
 
-        private void rect_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            _isRectDragInProg = true;
-            matr[0, 0].CaptureMouse();
-        }
-
-        private void rect_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
-        {
-            _isRectDragInProg = false;
-            matr[0, 0].ReleaseMouseCapture();
-        }
-
-        private void rect_MouseMove(object sender, MouseEventArgs e)
-        {
-            if (!_isRectDragInProg) return;
-
-            // get the position of the mouse relative to the Canvas
-            var mousePos = e.GetPosition(tavola);
-
-            // center the rect on the mouse
-            double left = mousePos.X - (matr[0, 0].ActualWidth / 2);
-            double top = mousePos.Y - (matr[0, 0].ActualHeight / 2);
-            double bottom = mousePos.Y - (matr[0, 0].ActualHeight / 2);
-            double right = mousePos.Y - (matr[0, 0].ActualHeight / 2);
-            if (left >= 502)
-            {
-                left = 502;
-            }
-            else if (top >= 408)
-            {
-                top = 408;
-            }
-            else if (top <= 0)
-            {
-                top = 0;
-            }
-            else if (left <= 0)
-            {
-                left = 0;
-            }
-            coordinate.Content = "x: " + top + " y: "+left;
-            Canvas.SetLeft(matr[0, 0], left);
-            Canvas.SetTop(matr[0, 0], top);
-        }
+        
 
     }
 }
